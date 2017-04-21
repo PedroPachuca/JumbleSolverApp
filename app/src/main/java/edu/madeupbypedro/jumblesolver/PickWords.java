6fdba38af6b33e59548a58295bd6557f2a8d2190
Pedro Pachuca <pedritopachuca@me.com>
2017-04-20 21:51:58 -0700

    Initial Commit

---
+++ app/src/main/java/edu/madeupbypedro/jumblesolver/PickWords.java
@@ -0,0 +1,13 @@
+package edu.madeupbypedro.jumblesolver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickWords extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_words);
+    }
+}
